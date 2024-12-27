43cc5185acaea1a52b279e2bf133434b1666d799
Robin Vestraete <robin.vestraete@gmail.com>
2024-12-27 16:35:50 +0000

    Screen Controller done.

--- assets/script.js
+++ assets/script.js
@@ -55,15 +55,18 @@ function GameController(playerOne = "Player 1", playerTwo = "Player 2") {
     const board = Gameboard;
     const rows = 3;
     let gameStatus = false;
+    let drawFlag = false;
 
     const players = [
         {
             name: playerOne,
-            item: 1
+            item: 1,
+            score: 0
         },
         {
             name: playerTwo,
-            item: 2
+            item: 2,
+            score: 0
         }
     ];
 
@@ -80,7 +83,14 @@ function GameController(playerOne = "Player 1", playerTwo = "Player 2") {
 
     };
 
+    const resetPlayersScores = () => {
+        players[0].score = 0;
+        players[1].score = 0;
+    };
+
     const getActivePlayer = () => activePlayer;
+
+    const getPlayers = () => players;
 
     const printNewRound = () => {
         board.printBoard();
@@ -104,17 +114,27 @@ function GameController(playerOne = "Player 1", playerTwo = "Player 2") {
         return false;
     };
 
+    let roundPlayed = 0;
+
     const playRound = (row,column) => {
         console.log(`Adding ${getActivePlayer().name}'s item into ${row}, ${column}...`);
 
         board.updateBoard(row,column,getActivePlayer().item);
 
+        roundPlayed++;
+
         if (checkWinner(row,column)){
 
             gameStatus = true;
 
+            activePlayer.score += 1;
+
             console.log(`The winner is ${getActivePlayer().name} !`);
             return
+
+        } else if (roundPlayed === 9){
+
+            drawFlag = true;
 
         } else {
             switchPlayerTurn();
@@ -123,10 +143,19 @@ function GameController(playerOne = "Player 1", playerTwo = "Player 2") {
     };
 
     const getGameStatus = () => gameStatus;
+    const getDrawFlag = () => drawFlag;
+    const resetGame = () => {
+        drawFlag = false;
+        gameStatus = false;
+        roundPlayed = 0;
+    };
 
     printNewRound();
 
-    return {playRound,getActivePlayer,getBoard: board.getBoard, getGameStatus, updatePlayersNames};
+    return {playRound,getActivePlayer,getBoard: board.getBoard, 
+        getGameStatus, updatePlayersNames, 
+        resetPlayersScores, resetGame,
+        getDrawFlag, getPlayers};
 
 };
 
@@ -138,6 +167,8 @@ function ScreenController () {
 
     const textContent = document.querySelector(".text-content");
     const gameBoard = document.querySelector(".game-board");
+    const endDialog = document.getElementById("dialog-end");
+    const endText = document.querySelector(".end-text");
 
     const updateBoard = () => {
 
@@ -157,13 +188,70 @@ function ScreenController () {
 
                 cellButton.dataset.row = indexRow;
                 cellButton.dataset.column = indexColumn;
-                cellButton.textContent = cell.getValue();
+
+                if (cell.getValue() === 1){
+                    cellButton.textContent = "X";
+                    cellButton.style.color = "red";
+                } else if (cell.getValue() === 2) {
+                    cellButton.textContent = "O";
+                    cellButton.style.color = "blue";
+                };
 
                 gameBoard.appendChild(cellButton);
 
             });
         });
+
+        if (game.getGameStatus()) {
+
+            endText.textContent = `${game.getActivePlayer().name} is the winner !`
+            endDialog.showModal();
+
+        };
+
+        if (game.getDrawFlag()) {
+
+            endText.textContent = `This is a draw !`
+            endDialog.showModal()
+        
+        };
     };
+
+    const resetBoard = () => {
+
+        gameBoard.textContent = "";
+
+        const board = game.getBoard();
+
+
+        board.forEach((row,indexRow) => {
+            row.forEach((cell,indexColumn) => {
+
+                const cellButton = document.createElement("button");
+                cellButton.classList.add("cell");
+
+                cellButton.dataset.row = indexRow;
+                cellButton.dataset.column = indexColumn;
+                cellButton.textContent = "";
+                cell.addItem(0);
+
+                gameBoard.appendChild(cellButton);
+
+            });
+        });
+
+        game.resetGame();
+        updateScoreDisplay();
+
+    };
+
+    const updateScoreDisplay = () => {
+
+        display.innerHTML = `<p> ${game.getPlayers()[0].name}` + `<span style="color: red;"> X </span> : ` +  `${game.getPlayers()[0].score} </p>` +
+        `<p> ${game.getPlayers()[1].name}` + `<span style="color: blue;"> O </span> : ` + `${game.getPlayers()[1].score} </p>`;
+
+    };
+
 
     // Handling of the clicks on board buttons
     function clickHandlerBoard(e) {
@@ -180,16 +268,17 @@ function ScreenController () {
 
     gameBoard.addEventListener("click", clickHandlerBoard);
 
-    updateBoard();
-
-
-      // Dialog handling 
+    // Dialog and side buttons handling 
 
       const addPlayers = document.querySelector(".add-players");
       const closeDialog = document.querySelector(".x");
       const player1 = document.getElementById("player-1");
       const player2 = document.getElementById("player-2");
       const playersForm = document.querySelector(".players-form");
+      const display = document.querySelector(".display");
+      const reset = document.querySelector(".stop-game");
+      const endForm = document.querySelector(".end-form");
+      const dialog = document.getElementById("dialog");
   
       addPlayers.addEventListener("click", () => {
           dialog.showModal();
@@ -207,12 +296,26 @@ function ScreenController () {
 
         game.updatePlayersNames(player1.value,player2.value);
 
+        updateScoreDisplay();
+
         updateBoard();
+
+        gameBoard.style.background = "#306754";
   
-          player1.value = "";
-          player2.value = "";
+        player1.value = "";
+        player2.value = "";
   
       });
+
+      reset.addEventListener("click", () => {
+
+        resetBoard();
+        game.resetPlayersScores();
+
+    });
+
+      endForm.addEventListener("submit",resetBoard);
+
 
 
 };
